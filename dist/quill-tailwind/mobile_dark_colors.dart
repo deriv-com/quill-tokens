fe833f2368df114eccf66281a35bdfccc98063b9
horam-deriv <64075768+horam-deriv@users.noreply.github.com>
2023-11-17 17:31:44 +0800

    add the first version of generated files

---
+++ dist/quill-tailwind/mobile_dark_colors.dart
@@ -0,0 +1,26 @@
+
+//
+// mobile_dark_colors.dart
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const base = #11141b;
+    static const base = #11141b;
+    static const container = #181c25;
+    static const container = #181c25;
+    static const danger = #f92e26;
+    static const default = rgba(#ffffff,72%);
+    static const dialog = #181c25;
+    static const disabled = rgba(#ffffff,24%);
+    static const info = #3daaff;
+    static const link = #ff444f;
+    static const prominent = #ffffff;
+    static const subtle = rgba(#ffffff,48%);
+    static const success = #26a44e;
+    static const warning = #ffae26;
+}
---
+++ dist/quill-tailwind/mobile_light_colors.dart
@@ -0,0 +1,26 @@
+
+//
+// mobile_light_colors.dart
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const base = #f6f7f8;
+    static const base = #ffffff;
+    static const container = #ffffff;
+    static const container = #f6f7f8;
+    static const danger = #db0800;
+    static const default = rgba(#000000,72%);
+    static const dialog = #ffffff;
+    static const disabled = rgba(#000000,24%);
+    static const info = #1789e1;
+    static const link = #ff444f;
+    static const prominent = #000000;
+    static const subtle = rgba(#000000,48%);
+    static const success = #00822a;
+    static const warning = #e18d00;
+}
